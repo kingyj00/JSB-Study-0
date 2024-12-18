c0633859fa95095db410d8860a94ccdf1c96de52
kingyj00 <onshopyj@gmail.com>
2024-12-18 10:54:20 +0900

    Step2-2rd-06

--- src/main/java/com/mysite/sbb/answer/Answer.java
+++ src/main/java/com/mysite/sbb/answer/Answer.java
@@ -1,9 +1,10 @@
-package com.mysite.sbb;
+package com.mysite.sbb.answer;
 
+import com.mysite.sbb.question.Question;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
+import com.mysite.sbb.question.Question;
 import java.time.LocalDateTime;
 import jakarta.persistence.ManyToOne;
 
